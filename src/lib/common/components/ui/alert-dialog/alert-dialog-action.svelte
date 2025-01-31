140a529b19ba87ed57386aee955be9185bfe8366
Santicm23 <santicastrom@outlook.com>
2025-01-30 21:48:14 -0500

    setting up frontend folder structure

--- src/lib/common/components/ui/alert-dialog/alert-dialog-action.svelte
+++ src/lib/common/components/ui/alert-dialog/alert-dialog-action.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { AlertDialog as AlertDialogPrimitive } from "bits-ui";
-	import { buttonVariants } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils.js";
+	import { buttonVariants } from "$lib/common/components/ui/button/index.js";
+	import { cn } from "@/common/utils.js";
 
 	type $$Props = AlertDialogPrimitive.ActionProps;
 	type $$Events = AlertDialogPrimitive.ActionEvents;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
-  import { Button } from "$lib/components/ui/button/index.js";
+  import * as AlertDialog from "$lib/common/components/ui/alert-dialog/index.js";
+  import { Button } from "$lib/common/components/ui/button/index.js";
  </script>
   
  <AlertDialog.Root>
