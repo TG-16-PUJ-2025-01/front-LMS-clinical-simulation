17b93a8ccbec7b1801fa65cfc158f27610ccc952
Santicm23 <santicastrom@outlook.com>
2025-02-05 20:39:54 -0500

    demo commit

--- src/modules/shared/auth/pages/Login/Login.tsx
+++ src/modules/shared/auth/pages/Login/Login.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/modules/core/components/ui/button";
+
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div>Login</div>
+      <Button>Button</Button>
+    </>
   )
 }
