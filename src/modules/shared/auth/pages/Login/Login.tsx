293f2a14b49d0a94f37a2e746877b8bc37ca40c7
Santicm23 <santicastrom@outlook.com>
2025-02-05 21:12:31 -0500

    adding demo components

--- src/modules/shared/auth/pages/Login/Login.tsx
+++ src/modules/shared/auth/pages/Login/Login.tsx
@@ -1,10 +1,17 @@
-import { Button } from "@/modules/core/components/ui/button";
+import { Button } from "@/modules/core/components/ui/button"
+import { Input } from "@/modules/core/components/ui/input"
 
 export default function Login() {
-  return (
-    <>
-      <div>Login</div>
-      <Button>Button</Button>
-    </>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center">
+			<form
+				onSubmit={() => {}}
+				className="flex w-1/2 max-w-80 flex-col items-center gap-4 rounded-lg border border-black bg-white p-4 shadow-md"
+			>
+				<Input placeholder="Correo"></Input>
+				<Input placeholder="Contraseña" type="password"></Input>
+				<Button className="w-full">Login</Button>
+			</form>
+		</main>
+	)
 }
