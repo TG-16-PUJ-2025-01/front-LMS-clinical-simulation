d222e1749115da9659f221bc3d715746082fe8ad
Santicm23 <santicastrom@outlook.com>
2025-02-15 09:13:44 -0500

    merging with rooms branch to bring sonner and navbar to videos page

--- src/modules/admin/rooms/pages/RoomsPage.tsx
+++ src/modules/admin/rooms/pages/RoomsPage.tsx
@@ -4,24 +4,21 @@ import { ComboboxSelect } from "@/modules/admin/rooms/components/combobox-select
 import { Toaster } from "@/modules/core/components/ui/sonner"
 import { useEffect, useState } from "react"
 import { addRoomType, getRoomsTypes } from "../services/roomService"
-import Room from "@/modules/core/models/room"
 import RoomType from "@/modules/core/models/roomType"
 
 export default function RoomsPage() {
-
 	const navLinks = [
 		{ label: "Listado de materias", href: "/admin/" },
 		{ label: "Listado de clases", href: "/admin/clases" },
 		{ label: "Listado de cuentas", href: "/admin/users" },
 		{ label: "Listado de salas", href: "#" },
-    	{ label: "Listado de videos", href: "/admin/videos" },
+		{ label: "Listado de videos", href: "/admin/videos" },
 	]
 	const [data, setData] = useState<RoomType[]>([])
 
 	const handleOnCreateOption = (option: { key: number; value: string }) => {
 		addRoomType(option.value)
 	}
-	
 
 	useEffect(() => {
 		const fetchRoomTypes = async () => {
@@ -34,25 +31,24 @@ export default function RoomsPage() {
 	return (
 		<>
 			<NavBar navLinks={navLinks} />
-			<main className="bg-gray-100 min-h-screen flex flex-col items-center py-8">
-				<div className="w-full max-w-6xl mb-4">
+			<main className="flex min-h-screen flex-col items-center bg-gray-100 py-8">
+				<div className="mb-4 w-full max-w-6xl">
 					<h1 className="text-2xl font-semibold">Salas</h1>
 				</div>
-				<div className="w-full max-w-6xl p-4 bg-white rounded-lg shadow-md flex flex-col justify-between h-full">
+				<div className="flex h-full w-full max-w-6xl flex-col justify-between rounded-lg bg-white p-4 shadow-md">
 					<div className="flex-grow">
 						<RoomsDataTable />
 					</div>
-					<ComboboxSelect 
-					options={data.map(roomType => ({ key: roomType.id, value: roomType.name }))} 
-					onCreateOption={handleOnCreateOption}
-					placeholderText="Seleccionar tipo..."
-					itemName="tipo de sala" 
-				/>
+					<ComboboxSelect
+						options={data.map((roomType) => ({ key: roomType.id, value: roomType.name }))}
+						onCreateOption={handleOnCreateOption}
+						placeholderText="Seleccionar tipo..."
+						itemName="tipo de sala"
+					/>
 				</div>
-				
+
 				<Toaster />
 			</main>
-			
 		</>
 	)
 }
