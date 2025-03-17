c764a881dce432de8f379b12776cbca0440ccb20
Rinconjr <nicorinconb@yahoo.com>
2025-03-16 23:56:10 -0500

    feat: enhance EditPracticeDialog with improved loading state and form structure; streamline practice attribute handling

--- src/modules/coordinator/practices/components/EditPracticeDialog.tsx
+++ src/modules/coordinator/practices/components/EditPracticeDialog.tsx
@@ -39,23 +39,15 @@ const formSchema = z.object({
 		message: "La descripción no puede estar vacía",
 	}),
 	gradeable: z.boolean(),
-	simulationDuration: z.number().int().min(1, {
-		message: "La duración de la simulación debe ser mayor o igual a 15",
-	}),
-	numberOfGroups: z.number().int().optional(),
-	maxStudentsGroup: z.number().int().optional(),
-	type: z.string().nonempty({ message: "El tipo no puede estar vacío" }),
 })
 
 export default function EditPracticeDialog({ open, onClose, practice }: Props) {
-	if (!practice) return null
-
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			name: "",
-			description: "",
-			gradeable: false,
+			name: practice?.name || "",
+			description: practice?.description || "",
+			gradeable: practice?.gradeable || false,
 		},
 	})
 
@@ -65,10 +57,6 @@ export default function EditPracticeDialog({ open, onClose, practice }: Props) {
 				name: practice.name,
 				description: practice.description,
 				gradeable: practice.gradeable,
-				simulationDuration: practice.simulationDuration,
-				numberOfGroups: practice.numberOfGroups ?? undefined,
-				maxStudentsGroup: practice.maxStudentsGroup ?? undefined,
-				type: practice.type.charAt(0) + practice.type.slice(1).toLowerCase(),
 			})
 		}
 	}, [form, practice])
@@ -76,17 +64,13 @@ export default function EditPracticeDialog({ open, onClose, practice }: Props) {
 	async function onSubmit(values: z.infer<typeof formSchema>) {
 		try {
 			const updatedPractice: Practice = {
-				id: practice.id,
+				...practice!,
 				name: values.name,
 				description: values.description,
-				type: practice.type,
 				gradeable: values.gradeable,
-				simulationDuration: practice.simulationDuration,
-				numberOfGroups: practice.numberOfGroups,
-				maxStudentsGroup: practice.maxStudentsGroup,
 			}
 
-			await updatePractice(practice.id, updatedPractice)
+			await updatePractice(practice!.id, updatedPractice)
 
 			toast.success("Práctica actualizada exitosamente.")
 
@@ -99,121 +83,37 @@ export default function EditPracticeDialog({ open, onClose, practice }: Props) {
 	return (
 		<Dialog open={open} onOpenChange={onClose}>
 			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Editar práctica</DialogTitle>
-					<DialogDescription>
-						Puedes editar los siguientes atributos de la sala.{" "}
-						<strong>
-							<div>
-								Si deseas modificar el tipo, número de grupos o duración de la simulación,{" "}
-								<u>debes crear otra práctica y eliminar esta.</u>
-							</div>
-						</strong>
-					</DialogDescription>
-				</DialogHeader>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-					<Form {...form}>
-						<div className="grid gap-4 py-4">
-							<FormField
-								control={form.control}
-								name="name"
-								render={({ field }) => (
-									<FormItem className="grid grid-cols-4 items-center gap-4">
-										<FormLabel className="m-0 text-right">Nombre</FormLabel>
-										<FormControl>
-											<Input id="name" placeholder="Nombre" className="col-span-3 m-0" {...field} />
-										</FormControl>
-										<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="description"
-								render={({ field }) => (
-									<FormItem className="grid grid-cols-4 items-center gap-4">
-										<FormLabel className="m-0 text-right">Descripcion</FormLabel>
-										<FormControl>
-											<Input
-												id="description"
-												placeholder="Descripcion"
-												className="col-span-3 m-0"
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="type"
-								render={({ field }) => (
-									<FormItem className="grid grid-cols-4 items-center gap-4">
-										<FormLabel className="m-0 text-right">Tipo</FormLabel>
-										<FormControl>
-											<Input id="type" className="col-span-3 m-0" {...field} disabled />
-										</FormControl>
-										<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="gradeable"
-								render={({ field }) => (
-									<FormItem className="grid grid-cols-4 items-center gap-4">
-										<FormLabel className="text-right">Evaluación</FormLabel>
-										<div className="col-span-3 flex items-center gap-2">
-											<FormControl>
-												<Checkbox
-													id="gradeable"
-													checked={field.value}
-													onCheckedChange={field.onChange}
-												/>
-											</FormControl>
-											<FormLabel htmlFor="gradeable">¿Práctica Evaluable?</FormLabel>
-										</div>
-										<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="simulationDuration"
-								render={({ field }) => (
-									<FormItem className="grid grid-cols-4 items-center gap-4">
-										<FormLabel className="m-0 text-right">Duración Simulación</FormLabel>
-										<FormControl>
-											<div className="col-span-3 flex items-center">
-												<Input
-													type="number"
-													value={field.value}
-													className="m-0 w-24 text-center"
-													disabled
-												/>
-												<span className="ml-2">min</span>
-											</div>
-										</FormControl>
-										<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
-									</FormItem>
-								)}
-							/>
-							{practice.type === "GRUPAL" && (
-								<>
+				{!practice ? (
+					<div>Cargando...</div>
+				) : (
+					<>
+						<DialogHeader>
+							<DialogTitle>Editar práctica</DialogTitle>
+							<DialogDescription>
+								Puedes editar los siguientes atributos de la sala.{" "}
+								<strong>
+									<span>
+										Si deseas modificar el tipo, número de grupos o duración de la simulación,{" "}
+										<u>debes crear otra práctica y eliminar esta.</u>
+									</span>
+								</strong>
+							</DialogDescription>
+						</DialogHeader>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+							<Form {...form}>
+								<div className="grid gap-4 py-4">
 									<FormField
 										control={form.control}
-										name="numberOfGroups"
+										name="name"
 										render={({ field }) => (
 											<FormItem className="grid grid-cols-4 items-center gap-4">
-												<FormLabel className="m-0 text-right">Número de grupos</FormLabel>
+												<FormLabel className="m-0 text-right">Nombre</FormLabel>
 												<FormControl>
 													<Input
-														type="number"
-														id="numberOfGroups"
+														id="name"
+														placeholder="Nombre"
 														className="col-span-3 m-0"
-														value={field.value ?? ""}
-														disabled
+														{...field}
 													/>
 												</FormControl>
 												<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
@@ -222,33 +122,100 @@ export default function EditPracticeDialog({ open, onClose, practice }: Props) {
 									/>
 									<FormField
 										control={form.control}
-										name="maxStudentsGroup"
+										name="description"
 										render={({ field }) => (
 											<FormItem className="grid grid-cols-4 items-center gap-4">
+												<FormLabel className="m-0 text-right">Descripcion</FormLabel>
+												<FormControl>
+													<Input
+														id="description"
+														placeholder="Descripcion"
+														className="col-span-3 m-0"
+														{...field}
+													/>
+												</FormControl>
+												<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name="gradeable"
+										render={({ field }) => (
+											<FormItem className="grid grid-cols-4 items-center gap-4">
+												<FormLabel className="text-right">Evaluación</FormLabel>
+												<div className="col-span-3 flex items-center gap-2">
+													<FormControl>
+														<Checkbox
+															id="gradeable"
+															checked={field.value}
+															onCheckedChange={field.onChange}
+														/>
+													</FormControl>
+													<FormLabel htmlFor="gradeable">¿Práctica Evaluable?</FormLabel>
+												</div>
+												<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
+											</FormItem>
+										)}
+									/>
+									<div className="grid grid-cols-4 items-center gap-4">
+										<FormLabel className="m-0 text-right">Tipo</FormLabel>
+										<Input
+											id="type"
+											value={
+												practice.type.charAt(0).toUpperCase() +
+												practice.type.slice(1).toLowerCase()
+											}
+											className="col-span-3 m-0"
+											disabled
+										/>
+									</div>
+									<div className="grid grid-cols-4 items-center gap-4">
+										<FormLabel className="m-0 text-right">Duración Simulación</FormLabel>
+										<div className="col-span-3 flex items-center">
+											<Input
+												type="number"
+												value={practice.simulationDuration}
+												className="m-0 w-24 text-center"
+												disabled
+											/>
+											<span className="ml-2">min</span>
+										</div>
+									</div>
+									{practice.type === "GRUPAL" && (
+										<>
+											<div className="grid grid-cols-4 items-center gap-4">
+												<FormLabel className="m-0 text-right">Número de grupos</FormLabel>
+												<Input
+													type="number"
+													id="numberOfGroups"
+													value={practice.numberOfGroups ?? ""}
+													className="col-span-3 m-0"
+													disabled
+												/>
+											</div>
+											<div className="grid grid-cols-4 items-center gap-4">
 												<FormLabel className="m-0 text-right">
 													Máximo estudiantes por grupo
 												</FormLabel>
-												<FormControl>
-													<Input
-														type="number"
-														id="maxStudentsGroup"
-														className="col-span-3 m-0"
-														value={field.value ?? ""}
-														disabled
-													/>
-												</FormControl>
-												<FormMessage className="col-span-4 m-0 -mt-2 text-right" />
-											</FormItem>
-										)}
-									/>
-								</>
-							)}
-						</div>
-						<DialogFooter>
-							<Button type="submit">Guardar</Button>
-						</DialogFooter>
-					</Form>
-				</form>
+												<Input
+													type="number"
+													id="maxStudentsGroup"
+													value={practice.maxStudentsGroup ?? ""}
+													className="col-span-3 m-0"
+													disabled
+												/>
+											</div>
+										</>
+									)}
+								</div>
+								<DialogFooter>
+									<Button type="submit">Guardar</Button>
+								</DialogFooter>
+							</Form>
+						</form>
+					</>
+				)}
 			</DialogContent>
 		</Dialog>
 	)
