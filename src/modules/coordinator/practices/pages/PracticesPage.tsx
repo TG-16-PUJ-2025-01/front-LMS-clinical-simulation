99a967d2a6cfc3ab844c1855b6e97635d9ce6efe
Rinconjr <nicorinconb@yahoo.com>
2025-03-26 22:47:52 -0500

    minor fix in typo

--- src/modules/coordinator/practices/pages/PracticesPage.tsx
+++ src/modules/coordinator/practices/pages/PracticesPage.tsx
@@ -72,7 +72,7 @@ export default function PracticesPage() {
 			</div>
 			<div className="flex justify-center">
 				{data.length === 0 ? (
-					<p className="text-gray-500">No se encontraron clases</p>
+					<p className="text-gray-500">No se encontraron prácticas</p>
 				) : (
 					<div className="grid grid-cols-1 gap-18 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
 						{data.map((practice) => (
