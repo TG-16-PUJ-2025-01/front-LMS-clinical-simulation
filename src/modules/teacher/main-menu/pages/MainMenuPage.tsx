42df4fd32e32a5f14a4ed6ed6cfc3e3da850f4ee
Rinconjr <nicorinconb@yahoo.com>
2025-03-27 21:17:41 -0500

    feat: implement filtering functionality in MainMenuDataTable; refactor to remove props from MainMenuPage

--- src/modules/teacher/main-menu/pages/MainMenuPage.tsx
+++ src/modules/teacher/main-menu/pages/MainMenuPage.tsx
@@ -1,29 +1,8 @@
 import NavBar from "@/modules/core/components/Headers/NavBar"
 import LayoutSlot from "@/modules/core/components/Slots/LayoutSlot"
-import { Combobox } from "@/modules/core/components/Combobox/Combobox"
 import { MainMenuDataTable } from "../components/MainMenuDataTable"
-import { useState } from "react"
-import { Button } from "@/modules/core/components/ui/button"
 
 export default function MainMenuPage() {
-	const currentYear = new Date().getFullYear()
-	const yearOptions = Array.from({ length: 5 }, (_, i) => ({
-		key: currentYear - i,
-		value: (currentYear - i).toString(),
-	}))
-	const periodOptions = ["1", "2", "3"].map((period) => ({
-		key: Number(period),
-		value: period,
-	}))
-
-	const [selectedYear, setSelectedYear] = useState<number | null>(null)
-	const [selectedPeriod, setSelectedPeriod] = useState<number | null>(null)
-
-	const resetFilters = () => {
-		setSelectedYear(null)
-		setSelectedPeriod(null)
-	}
-
 	return (
 		<>
 			<LayoutSlot name="header">
@@ -37,27 +16,7 @@ export default function MainMenuPage() {
 				/>
 			</LayoutSlot>
 			<LayoutSlot name="title">Tus Clases</LayoutSlot>
-			<div className="mb-4 flex justify-end items-center gap-4">
-				<Combobox
-					placeholderText="Año"
-					options={yearOptions}
-					itemName="año"
-					selectedValue={selectedYear ? selectedYear.toString() : ""}
-					onChange={(selected) => setSelectedYear(selected.value ? Number(selected.value) : null)}
-				/>
-				<span className="text-xl font-bold">-</span>
-				<Combobox
-					placeholderText="Periodo"
-					options={periodOptions}
-					itemName="periodo"
-					selectedValue={selectedPeriod ? selectedPeriod.toString() : ""}
-					onChange={(selected) => setSelectedPeriod(selected.value ? Number(selected.value) : null)}
-				/>
-				<Button variant="default" onClick={resetFilters}>
-					Resetear Filtros
-				</Button>
-			</div>
-			<MainMenuDataTable selectedYear={selectedYear} selectedPeriod={selectedPeriod} />
+			<MainMenuDataTable />
 		</>
 	)
 }
--- src/modules/teacher/main-menu/services/MainMenuService.ts
+++ src/modules/teacher/main-menu/services/MainMenuService.ts
@@ -3,13 +3,17 @@ import ApiResponse from "@/modules/core/models/apiResponse"
 import Class from "@/modules/core/models/class"
 import axios from "axios"
 
-export async function getMenuInfo(year?: number, period?: number): Promise<ApiResponse<Class[]>> {
-	const params = new URLSearchParams()
-	if (year !== undefined && year !== null) params.append("year", year.toString())
-	if (period !== undefined && period !== null) params.append("period", period.toString())
-
-	const url = `${API_URL}/class/all/professor${params.toString() ? `?${params.toString()}` : ""}`
-    
-	const { data } = await axios.get(url)
+export async function getMenuInfo(
+	year?: number,
+	period?: number,
+	filter: string = ""
+): Promise<ApiResponse<Class[]>> {
+	const { data } = await axios.get(`${API_URL}/class/all/professor`, {
+		params: {
+			year,
+			period,
+			filter,
+		},
+	})
 	return data
 }
