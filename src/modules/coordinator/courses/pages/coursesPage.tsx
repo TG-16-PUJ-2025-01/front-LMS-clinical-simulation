76a6d63829756b510707698ddfbc09abbffbde2b
Santicm23 <santicastrom@outlook.com>
2025-03-30 16:29:26 -0500

    minor fixes

--- src/modules/coordinator/courses/pages/coursesPage.tsx
+++ src/modules/coordinator/courses/pages/coursesPage.tsx
@@ -3,24 +3,24 @@ import { CoursesDataTable } from "../components/CoursesDataTable"
 import NavBar from "@/modules/core/components/Headers/NavBar"
 
 export default function coursesPage() {
-    return (
-        <>
-            <LayoutSlot name="header">
-                            <NavBar
-                                navLinks={[
-                                    {
-                                        label: "Calendario",
-                                        href: "/calendario",
-                                    },
-                                    {
-                                        label: "Página de inicio de la clase",
-                                        href: `/coordinador/asignaturas`,
-                                    },
-                                ]}
-                        />
-                        </LayoutSlot>
-            <LayoutSlot name="title">Mis Asignaturas</LayoutSlot>
-            <CoursesDataTable />
-        </>
-    )
+	return (
+		<>
+			<LayoutSlot name="header">
+				<NavBar
+					navLinks={[
+						{
+							label: "Calendario",
+							href: "/calendario",
+						},
+						{
+							label: "Página de inicio de la clase",
+							href: `/coordinador/asignaturas`,
+						},
+					]}
+				/>
+			</LayoutSlot>
+			<LayoutSlot name="title">Mis Asignaturas</LayoutSlot>
+			<CoursesDataTable />
+		</>
+	)
 }
