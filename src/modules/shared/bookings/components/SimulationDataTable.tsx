cf3fabf6f8c6b1cff3d26a78c77e9a517d21e738
Nicolás Rincón <nicolas.rincon@caseware.com>
2025-04-16 22:34:27 -0500

    feat: pass maxStudents prop to ViewMembersDialog in SimulationDataTable

--- src/modules/shared/bookings/components/SimulationDataTable.tsx
+++ src/modules/shared/bookings/components/SimulationDataTable.tsx
@@ -368,6 +368,7 @@ export function SimulationDataTable({ practice }: Props) {
 				open={isViewMembersOpen}
 				onClose={() => setIsViewMembersOpen(false)}
 				simulationId={selectedSimulation?.simulationId ?? 0}
+				maxStudents={practice.maxStudentsGroup ?? 0}
 			/>
 			<AssignRubricDialog
 				open={isAssignRubricOpen}
