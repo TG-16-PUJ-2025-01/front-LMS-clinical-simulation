85c18dcffa3ae64205c8802bd68b7d4246fcb4bf
Rinconjr <nicorinconb@yahoo.com>
2025-04-24 21:58:58 -0500

    feat: rename test cases for clarity and add unauthorized access scenario

--- cypress/e2e/profesor.cy.ts
+++ cypress/e2e/profesor.cy.ts
@@ -1,5 +1,6 @@
 describe('Teacher flow tests', () => {
-  it('Login', () => {
+
+  it('PROF-1 Flujo Completo Profe', () => {
     cy.visit('/');
 
     cy.wait(1000);
@@ -12,4 +13,18 @@ describe('Teacher flow tests', () => {
 
     cy.url().should('include', '/asignaturas');
   });
+
+  it ('PROF-2 Acceso No Autorizado Profesor', () => {
+    cy.visit('/');
+
+    cy.wait(1000);
+
+    cy.get('#email').type('profesor@gmail.com');
+
+    cy.get('#password').type('profesor');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/asignaturas');
+  })
 });
