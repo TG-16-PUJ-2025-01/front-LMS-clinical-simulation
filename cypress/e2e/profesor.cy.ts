8b75b3de54cc9bc74e69d8915c8058b958a88a07
Rinconjr <nicorinconb@yahoo.com>
2025-04-25 23:21:59 -0500

    chore: add cypress-plugin-steps dependency to enhance Cypress testing capabilities

--- cypress/e2e/profesor.cy.ts
+++ cypress/e2e/profesor.cy.ts
@@ -13,21 +13,90 @@ describe('Teacher flow tests', () => {
 
     cy.get('button[type="submit"]').click();
 
-    cy.url().should('include', '/asignaturas');
+    cy.url().should('include', '/profesor/asignaturas');
   });
 
-  it ('PROF-2 Acceso No Autorizado Profesor', () => {
+  it('PROF-2 Acceso No Autorizado Profesor', () => {
+
+    cy.step('Step 1 - Login');
     cy.visit('/');
-
     cy.wait(1000);
-
     cy.get('#email').type('profesor@gmail.com');
-
     cy.get('#password').type('profesor');
-
     cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/profesor/asignaturas');
 
-    cy.url().should('include', '/asignaturas');
+    cy.step('Step 2 - Revisar acceso a clase autorizado');
+    cy.visit('/profesor/clases/1/practicas');
+    cy.url().should('include', '/profesor/clases/1/practicas');
+    cy.get('h1').invoke('text').should('contain', '(20001) Semiología Clínica - 2025-10');
+
+    cy.step('Step 3 - Revisar acceso a clase no autorizado');
+    cy.visit('/profesor/clases/3/practicas');
+    cy.url().should('include', '/login');
+    // cy.get('h1').invoke('text').should('contain', 'Iniciar Sesión'); TODO arreglar validacion
+
+    cy.step('Step 4 - Revisar acceso a practicas autorizadas');
+    cy.get('#email').type('profesor@gmail.com');
+    cy.get('#password').type('profesor');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/profesor/asignaturas');
+    cy.visit('/profesor/clases/1/practicas/1');
+    cy.url().should('include', '/profesor/clases/1/practicas/1');
+    cy.get('h1').invoke('text').should('contain', 'Practica 1');
+
+    cy.step('Step 5 - Revisar acceso a practicas no autorizadas');
+    cy.visit('/profesor/clases/1/practicas/5');
+    cy.url().should('include', '/login');
+    // cy.get('h1').invoke('text').should('contain', 'Iniciar Sesión'); TODO arreglar validacion
+
+    cy.step('Step 6 - Revisar acceso a listado de calificaciones autorizadas');
+    cy.get('#email').type('profesor@gmail.com');
+    cy.get('#password').type('profesor');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/profesor/asignaturas');
+    cy.visit('/profesor/clases/1/calificaciones');
+    cy.url().should('include', '/profesor/clases/1/calificaciones');
+    cy.get('h1').invoke('text').should('contain', 'Calificaciones');
+
+    cy.step('Step 7 - Revisar acceso a listado de calificaciones no autorizadas');
+    cy.visit('/profesor/clases/3/calificaciones');
+    cy.url().should('include', '/login');
+    // cy.get('h1').invoke('text').should('contain', 'Iniciar Sesión'); TODO arreglar validacion
+
+    cy.step('Step 8 - Revisar acceso a listado de miembros autorizadas');
+    cy.get('#email').type('profesor@gmail.com');
+    cy.get('#password').type('profesor');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/profesor/asignaturas');
+    cy.visit('/profesor/clases/1/miembros');
+    cy.url().should('include', '/profesor/clases/1/miembros');
+    cy.get('h1').invoke('text').should('contain', 'Miembros de la Clase');
+
+    cy.step('Step 9 - Revisar acceso a listado de miembros no autorizadas');
+    cy.pause();
+    cy.visit('/profesor/clases/3/miembros');
+    cy.url().should('include', '/login');
+    // cy.get('h1').invoke('text').should('contain', 'Iniciar Sesión'); TODO arreglar validacion
+
+    cy.step('Step 10 - Revisar acceso a listado de simulaciones autorizadas');
+    cy.get('#email').type('profesor@gmail.com');
+    cy.get('#password').type('profesor');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/profesor/asignaturas');
+    cy.visit('/profesor/simulacion/1');
+    cy.url().should('include', '/profesor/simulacion/1');
+    cy.get('h1').invoke('text').should('contain', 'Practica 1 (Grupo 1)');
+
+    cy.step('Step 11 - Revisar acceso a listado de simulaciones no autorizadas');
+    cy.visit('/profesor/simulacion/16'); // TODO: Revisar si la simulacion 16 existe
+    cy.url().should('include', '/login');
+    // cy.get('h1').invoke('text').should('contain', 'Iniciar Sesión'); TODO arreglar validacion
+
+    cy.step('Step 12 - Revisar no acceso si no se ha iniciado sesion');
+    cy.visit('/profesor/asignaturas');
+    cy.url().should('include', '/login');
+    // cy.get('h1').invoke('text').should('contain', 'Iniciar Sesión'); TODO arreglar validacion
   })
 
   /*
