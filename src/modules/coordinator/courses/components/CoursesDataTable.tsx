8721404d45b8624d25593fb8d48772287b4f7f6c
Rinconjr <nicorinconb@yahoo.com>
2025-05-01 20:49:44 -0500

    Refactor CoursesDataTable component to improve filter functionality and UI; add reset filters button and year options fetching

--- src/modules/coordinator/courses/components/CoursesDataTable.tsx
+++ src/modules/coordinator/courses/components/CoursesDataTable.tsx
@@ -13,114 +13,144 @@ import { CardClass } from "./CardClass"
 import { getCoordinatorCourses } from "../services/courseService"
 import CourseDTO from "../dtos/courseDto"
 import { Combobox } from "@/modules/core/components/Combobox/Combobox"
+import { Button } from "@/modules/core/components/ui/button"
 
 export function CoursesDataTable() {
-	const [filter, setFilter] = useState<string>("")
+	const navigate = useNavigate()
+	const [yearOptions, setYearOptions] = useState<{ key: number; value: string }[]>([])
+	const periodOptions = ["10", "20", "30"].map((period) => ({
+		key: Number(period),
+		value: period,
+	}))
+	const searchBy = ["Asignaturas", "Clases", "Profesores"].map((by, index) => ({
+		key: index,
+		value: by,
+	}))
+
 	const [data, setData] = useState<CourseDTO[]>([])
 	const [searchByKey, setSearchByKey] = useState<string>("")
-	const [year, setYear] = useState<string>("")
-	const [period, setPeriod] = useState<string>("")
-	const navigate = useNavigate()
+	const [selectedYear, setSelectedYear] = useState<string>("")
+	const [selectedPeriod, setSelectedPeriod] = useState<string>("")
+	const [filter, setFilter] = useState<string>("")
 
-	const searchBy = ["Asignaturas", "Clases", "Profesores"]
-	const periodList = ["1", "2", "3", ""]
-	const currentYear = new Date().getFullYear()
-	const yearList = Array.from({ length: 21 }, (_, i) => currentYear - i)
+
+	const resetFilters = () => {
+		setSelectedYear("")
+		setSelectedPeriod("")
+		setFilter("")
+		setSearchByKey("")
+	}
 
 	useEffect(() => {
 		const fetchCoursesAndClasses = async () => {
-			// Here we should fetch the courses and classes
-			// using the filter and pagination values
-			// and update the UI with the results
-
 			let searchPeriod = ""
-			if (year != "" && period === "") {
-				searchPeriod = year
-			} else if (year === "" && period != "") {
-				searchPeriod = "-" + period
-			} else if (year != "" && period != "") {
-				searchPeriod = year + "-" + period
+			if (selectedYear != "" && selectedPeriod === "") {
+				searchPeriod = selectedYear
+			} else if (selectedYear === "" && selectedPeriod != "") {
+				searchPeriod = "-" + selectedPeriod
+			} else if (selectedYear != "" && selectedPeriod != "") {
+				searchPeriod = selectedYear + "-" + selectedPeriod
 			} else {
 				searchPeriod = ""
 			}
 
 			const res = await getCoordinatorCourses(searchByKey, filter, searchPeriod, true)
 
-			//revisar si se deberia incluir la lista de res.data. quitar del array la info de los courses con clases en 0
-			if (year != "" || period != "" || searchByKey != "") {
+			if (selectedYear != "" || selectedPeriod != "" || searchByKey != "") {
 				res.data = res.data.filter((element) => element.classes.length > 0)
 			}
 
 			setData(res.data)
+
+			console.log("Data fetched:", res.data)
 		}
 
 		fetchCoursesAndClasses()
-	}, [filter, searchByKey, period, year])
+	}, [filter, searchByKey, selectedYear, selectedPeriod])
+
+	useEffect(() => {
+		const fetchYearOptions = async () => {
+			try {
+				const res = await getCoordinatorCourses("", "", "", false)
+				const years = res.data
+                .flatMap((course: CourseDTO) => 
+                    course.classes.map((classItem) => parseInt(classItem.period.split("-")[0]))
+                );
+				const oldestYear = Math.min(...years)
+				const newestYear = Math.max(...years)
+				const generatedYearOptions = Array.from(
+					{ length: newestYear - oldestYear + 1 },
+					(_, i) => ({
+						key: newestYear - i,
+						value: (newestYear - i).toString(),
+					})
+				)
+				setYearOptions(generatedYearOptions)
+
+				console.log("Year options fetched:", generatedYearOptions)
+			} catch (error) {
+				console.error("Error fetching classes:", error)
+			}
+		}
+
+		fetchYearOptions()
+	}, [])
 
 	return (
 		<>
-			<div className="flex items-center space-x-4">
-				<Combobox
-					placeholderText="Año"
-					options={yearList.map((key, index) => ({
-						key: index,
-						value: key.toString(),
-					}))}
-					itemName="por"
-					onChange={(selected) => {
-						if (selected?.value === year) {
-							setYear("") // Deselecciona si se selecciona lo mismo dos veces
-						} else {
-							setYear(selected?.value.toString() ?? "") // Actualiza el valor
-						}
-					}}
-				/>
-
-				<Combobox
-					placeholderText="Periodo"
-					options={periodList.map((key, index) => ({
-						key: index,
-						value: key,
-					}))}
-					itemName="por"
-					onChange={(selected) => {
-						if (selected?.value === period) {
-							setPeriod("") // Deselecciona si se selecciona lo mismo dos veces
-						} else {
-							setPeriod(selected?.value.toString() ?? "") // Actualiza el valor
-						}
-					}}
-				/>
-
-				<Combobox
-					placeholderText="Buscar por..."
-					options={searchBy.map((key, index) => ({
-						key: index, // Unique numeric key
-						value: key,
-					}))}
-					itemName="por"
-					onChange={(selected) => {
-						if (selected?.value === searchByKey) {
-							setSearchByKey("") // Deselecciona si se selecciona lo mismo dos veces
-						} else {
-							setSearchByKey(selected?.value || "") // Actualiza el valor
-						}
-					}}
-				/>
-
-				<div className="relative w-1/2 max-w-sm">
-					<Search className="absolute top-1/2 left-2.5 h-4 w-4 -translate-y-1/2 stroke-zinc-500" />
-					<Input
-						placeholder="Buscar..."
-						value={filter}
-						onChange={(event) => setFilter(event.target.value)}
-						className="w-full pl-8"
-						disabled={searchByKey === ""}
+			<div className="mb-4 flex items-center justify-between gap-4">
+				<div className="flex w-1/2 items-center gap-4">
+					<Combobox
+						placeholderText="Buscar por..."
+						options={searchBy}
+						itemName="por"
+						selectedValue={searchBy ? searchByKey : ""}
+						onChange={(selected) => {
+							if (selected?.value === searchByKey) {
+								setSearchByKey("")
+							} else {
+								setSearchByKey(selected?.value || "")
+							}
+						}}
 					/>
+					<div className="relative w-full">
+						<Search className="absolute top-1/2 left-2.5 h-4 w-4 -translate-y-1/2 stroke-zinc-500" />
+						<Input
+							placeholder="Buscar..."
+							value={filter}
+							onChange={(event) => setFilter(event.target.value)}
+							className="w-full pl-8"
+							disabled={searchByKey === ""}
+						/>
+					</div>
+				</div>
+				<div className="flex items-center gap-4">
+					<Combobox
+						placeholderText="Año"
+						options={yearOptions}
+						selectedValue={selectedYear ? selectedYear.toString() : ""}
+						itemName="año"
+						onChange={(selected) => {
+							setSelectedYear(selected?.value.toString() ?? "")
+						}}
+					/>
+					<span className="text-xl font-bold">-</span>
+					<Combobox
+						placeholderText="Periodo"
+						options={periodOptions}
+						selectedValue={selectedPeriod ? selectedPeriod.toString() : ""}
+						itemName="periodo"
+						onChange={(selected) => {
+							setSelectedPeriod(selected?.value.toString() ?? "")
+						}}
+					/>
+					<Button variant="default" onClick={resetFilters}>
+						Reiniciar Filtros
+					</Button>
 				</div>
 			</div>
 
-			<div className="mt-8 w-full min-h-[60vh] flex flex-col gap-4">
+			<div className="mt-4 flex min-h-[60vh] w-full flex-col gap-4">
 				{data.length === 0 && (
 					<div className="flex h-full items-center justify-center">
 						<p className="text-center text-gray-500">No hay resultados</p>
@@ -129,22 +159,22 @@ export function CoursesDataTable() {
 				{data.map((course) => (
 					<div key={course.courseId}>
 						<div className="flex items-center space-x-4">
-							<h1 className="text-xl font-bold">{course.name}</h1> {/* Nombre de la asignatura */}
+							<h1 className="text-lg font-bold">{course.name}</h1>{" "}
 						</div>
 
 						<div className="rounded-md p-2">
 							<div className="relative">
-								{course.classes.length > 0 ? ( // Si hay clases, renderiza el carrusel
+								{course.classes.length > 0 ? (
 									<Carousel className="relative">
 										<CarouselContent className="px-12 py-4">
 											{course.classes.map((classItem) => (
 												<CarouselItem key={classItem.classId}>
 													<CardClass
-														title={`(${classItem.javerianaId.toString()}) ${classItem.period}`} // Nombre de la clase
-														professor={classItem.professors.map((prof) => prof.name).join(", ")} // Lista de profesores separados por comas
+														title={`(${classItem.javerianaId.toString()}) ${classItem.period}`}
+														professor={classItem.professors.map((prof) => prof.name).join(", ")}
 														onClick={() =>
 															navigate(`/coordinador/clases/${classItem.classId}/practicas`)
-														} // Navega a la página de prácticas
+														}
 													/>
 												</CarouselItem>
 											))}
