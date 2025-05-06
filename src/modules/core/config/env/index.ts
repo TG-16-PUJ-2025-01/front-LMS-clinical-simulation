3355dfbbe771dc183b9a663580b3a534156f8aad
santicm23 <santicastrom@outlook.com>
2025-05-05 23:43:51 -0500

    adding api url to .env for easier deployment

--- src/modules/core/config/env/index.ts
+++ src/modules/core/config/env/index.ts
@@ -1,3 +1,1 @@
-// TODO: Import the env variables from the .env file
-
-export const API_URL = "http://localhost:8080"
+export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8080"
