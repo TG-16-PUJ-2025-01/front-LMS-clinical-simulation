079184c7fdc2083d06c947bcbf45b98d4e79b2ec
Rinconjr <nicorinconb@yahoo.com>
2025-05-22 21:58:16 -0500

    Adding new student flow test

--- cypress/e2e/estudiante.cy.ts
+++ cypress/e2e/estudiante.cy.ts
@@ -1,5 +1,86 @@
 describe("Estudiante flow test", () => {
-	it.only("EST-1 Flujo completo de un estudiante", () => {
+	it("EST-2 Acceso No Autorizado Estudiante", () => {
+		cy.step("Step 1 - Login")
+		cy.visit("/")
+		cy.get("input#email").should("be.visible").and("have.attr", "placeholder", "Correo registrado")
+		cy.get("input#password").should("be.visible").and("have.attr", "placeholder", "Contraseña")
+		cy.get('button[type="submit"]').should("be.visible").and("contain", "Ingresar")
+		cy.get("#email").type("estudiante@gmail.com")
+		cy.get("#password").type("estudiante")
+		cy.get('button[type="submit"]').click()
+		cy.url().should("include", "/estudiante/asignaturas")
+
+		cy.step("Step 2 - Acceso autorizado a clase 1")
+		cy.visit("/estudiante/clases/1/practicas")
+		cy.url().should("include", "/estudiante/clases/1/practicas")
+		cy.get("h1").invoke("text").should("contain", "(20001) Semiología Clínica - 2025-10")
+
+		cy.step("Step 3 - Acceso NO autorizado a clase 2")
+		cy.visit("/estudiante/clases/2/practicas")
+		cy.url().should("include", "/login")
+		cy.get("input#email").should("be.visible")
+		cy.get("input#password").should("be.visible")
+		cy.get('button[type="submit"]').should("be.visible")
+
+		cy.step("Step 4 - Acceso autorizado a práctica 1 de clase 1")
+		cy.get("#email").type("estudiante@gmail.com")
+		cy.get("#password").type("estudiante")
+		cy.get('button[type="submit"]').click()
+		cy.url().should("include", "/estudiante/asignaturas")
+		cy.visit("/estudiante/simulacion/1")
+		cy.url().should("include", "/estudiante/simulacion/1")
+		cy.get("h1").invoke("text").should("contain", "Práctica 1")
+
+		cy.step("Step 5 - Acceso NO autorizado a video ajeno de clase 1")
+		cy.visit("/estudiante/simulacion/2")
+		cy.url().should("include", "/login")
+		cy.get("input#email").should("be.visible")
+		cy.get("input#password").should("be.visible")
+		cy.get('button[type="submit"]').should("be.visible")
+
+		cy.step("Step 6 - Acceso autorizado a calificaciones de clase 1")
+		cy.get("#email").type("estudiante@gmail.com")
+		cy.get("#password").type("estudiante")
+		cy.get('button[type="submit"]').click()
+		cy.url().should("include", "/estudiante/asignaturas")
+		cy.visit("/estudiante/clases/1/calificaciones")
+		cy.url().should("include", "/estudiante/clases/1/calificaciones")
+		cy.get("h1").invoke("text").should("contain", "Calificaciones")
+
+		cy.step("Step 7 - Acceso NO autorizado a calificaciones de clase 2")
+		cy.visit("/estudiante/clases/2/calificaciones")
+		cy.url().should("include", "/login")
+		cy.get("input#email").should("be.visible")
+		cy.get("input#password").should("be.visible")
+		cy.get('button[type="submit"]').should("be.visible")
+
+		cy.step("Step 8 - Acceso autorizado a miembros de clase 1")
+		cy.get("#email").type("estudiante@gmail.com")
+		cy.get("#password").type("estudiante")
+		cy.get('button[type="submit"]').click()
+		cy.url().should("include", "/estudiante/asignaturas")
+		cy.visit("/estudiante/clases/1/miembros")
+		cy.url().should("include", "/estudiante/clases/1/miembros")
+		cy.get("h1").invoke("text").should("contain", "Miembros de la Clase")
+
+		cy.step("Step 9 - Acceso NO autorizado a miembros de clase 2")
+		cy.clearCookies()
+		cy.clearLocalStorage()
+		cy.visit("/estudiante/clases/2/miembros")
+		cy.url().should("include", "/login")
+		cy.get("input#email").should("be.visible")
+		cy.get("input#password").should("be.visible")
+		cy.get('button[type="submit"]').should("be.visible")
+
+		cy.step("Step 10 - Acceso NO autorizado si no ha iniciado sesión")
+		cy.visit("/estudiante/asignaturas")
+		cy.url().should("include", "/login")
+		cy.get("input#email").should("be.visible")
+		cy.get("input#password").should("be.visible")
+		cy.get('button[type="submit"]').should("be.visible")
+	})
+
+	it("EST-1 Flujo completo de un estudiante", () => {
 		cy.step("Step 1 - Login")
 		cy.visit("/")
 		cy.wait(2000)
@@ -153,21 +234,21 @@ describe("Estudiante flow test", () => {
 			.should("exist") // Verifica que existe
 			.parents("button") // Sube al botón contenedor
 			.click() // Hace clic en el botón
-		
+
 		cy.get('[role="menu"]') // Selecciona el contenedor del menú
 			.contains("Unirse a Grupo") // Busca el menú item que contiene ese texto
 			.click() // Hace clic
-			
+
 		cy.get('input[placeholder="Buscar por número de grupo..."]')
 			.should("be.visible") // Espera a que el input esté visible
 			.type("7")
-		
+
 		cy.get("tbody tr")
 			.eq(0) // la fila 3 (índice 2 porque empieza en 0)
 			.find("td")
 			.eq(3) // la primera columna de esa fila
 			.click() // la primera columna de esa fila
-			
+
 		cy.get('li[data-sonner-toast][data-visible="true"]')
 			.should("exist")
 			.and("contain.text", "Inscrito en el grupo")
@@ -182,7 +263,6 @@ describe("Estudiante flow test", () => {
 			.should("be.visible") // Espera a que el input esté visible
 			.clear()
 
-	
 		cy.get('input[placeholder="Buscar por número de grupo..."]')
 			.should("be.visible") // Espera a que el input esté visible
 			.type("7")
@@ -192,7 +272,6 @@ describe("Estudiante flow test", () => {
 			.find("td")
 			.eq(3) // la primera columna de esa fila
 			.should("contain.text", "Inscrito")
-
 
 		cy.get('[role="dialog"]')
 			.find("button")
